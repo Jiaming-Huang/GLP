954e7be97bace0bd364c42f5db366e6f82a6c905
Jiaming Huang <jiaming.huang@upf.edu>
2022-04-13 16:32:49 +0200

    misc

--- routines/params.m
+++ routines/params.m
@@ -1,10 +1,10 @@
-function par = params(K0,parchoice)
+function par = params(G0,parchoice)
 % phi controls persistence
 % bet controls size
 % parchoice - 1, same size different persistence
 %           - 2, different size same persistence
 
-if K0 == 2
+if G0 == 2
     if parchoice == 1
         bet       = [3 3];
         phi        = [0.2 0.6];
@@ -12,24 +12,13 @@ if K0 == 2
         bet         = [1 2];
         phi         = [0.5 0.5];
     end
-elseif K0 == 3
+elseif G0 == 3
     if parchoice == 1
         bet        = [3 3 3];
         phi        = [0.2, 0.6, 0.9];        
     elseif parchoice == 2
         bet        = [1 2 3];
         phi        = [0.5, 0.5, 0.5];
-    else
-        bet        = [0.4 0.6 0.8];
-        phi        = [0.4, 0.4, 0.4];
-    end
-else
-    if parchoice == 1
-        bet        = [3 3 3 3];
-        phi        = [0.2, 0.4, 0.7, 0.9];        
-    else
-        bet        = [1 2 3 4];
-        phi        = [0.5, 0.5, 0.5 0.5];
     end
 end
 
