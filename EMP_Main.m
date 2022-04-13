4fa31a446b796b72944e5c9b0b56910f5392e2cd
Jiaming Huang <jiaming.huang@upf.edu>
2022-04-13 16:17:25 +0200

    Sec 7.2 updated GLP algorithm

--- EMP_Main.m
+++ EMP_Main.m
@@ -1,19 +1,19 @@
-% Group Local Projection
-% Version: 2021/03/08
-% Author: Jiaming Huang
-% There are four possibilities (change line 33 and 38)
-%       - FE: lagged y - potential dynamic panel bias (but T is relatively large)
-%       - FE: no lagged y - then why do FE? all rhs variables are aggregates
-%       - RE: no lagged y - least efficient specification
-%       - RE: lagged y - potential violation of RE assumption
-
-
-close all; clear all; clc;
-addpath('./routines');
-
 %% Empirical Application
+% See Section 7 in the paper
+% Baseline Model (FE)
+% y_it+h = mu_i + x_t*bet_gh + c_it*phi_ih + eps_it+h
+% y_it+h    - housing inflation (log dif hp *100)
+% xt        - FFR
+% cit       - 4 lags of y, x, 30-year mortgage rates (FRM30), industrial production growth (GIP), PCE inflation (INFL_PCE), growth of real estate loans (reloan)
+% zt        - info robust high-frequency MP shocks Miranda-Agrippino & Ricco
+
+%% Housekeeping
+% close all;
+% clear; clc;
+% addpath('./routines');
+
 %% Load Data
-% dum         = importdata('..\data\empirical_main.csv');
+% dum         = importdata('./data/empirical_main.csv');
 % data        = dum.data;
 % date        = dum.textdata(2:end,1);
 % MSA         = dum.textdata(2:end,2);
@@ -23,176 +23,155 @@ addpath('./routines');
 % par.date    = date(1:par.Tfull,1);
 % MSA         = reshape(MSA,par.Tfull,par.N);
 % clear dum
-% save('./output/EMP_data.mat');
+% save('./data/EMP_data.mat');
 
-load('./output/EMP_data.mat');
-
+load('./data/EMP_data.mat');
+rng(27);
 %% Model Specification
 % 1. feed the index of your x, y and z
 % 2. specify the number of lags
 FE = 1;
-par.y_idx = 18;         % housing inflation, log difference *100
+par.y_idx = 17;         % housing inflation, log difference *100
 par.x_idx = 8;          % FFR - 8; GS1 - 9
-par.w_idx = [16 20 23 25];    %  FRM30, GIP, INFL_PCE, reloan
-par.z_idx = [13];       % FF4 - 12; info robust IV1 - 13; MPSign - 14; RR - 15; 
-par.nylag = 4;
+par.c_idx = [16 19 22 24];    %  FRM30, GIP, GREALLN, INFL_PCE
+par.z_idx = [13];       % FF4 - 12; info robust IV1 - 13; MPSign - 14; RR - 15;
+par.nylag = nylag;
 par.nxlag = 4;
-par.nwlag = 4;
+par.nclag = 4;
 par.nzlag = 0;
 par.horizon = 24;       % horizons
 par.nwtrunc = 25;
 par.start = '1975-01-01';
 par.end   = '2007-12-01';
-reg = prepare_GLP(data, par);
+reg = preEmpData(data, par);
 
 tmp = reg;
 tmp.LHS = cumsum(tmp.LHS,2);  % recover level
 
+spec = 'FE';
+if par.nylag == 0
+    spec = strcat(spec,'_NoYlag_');
+else
+    spec = strcat(spec,'_Ylag_');
+end
+
 %% Now you have three options: individual LP, panel LP or GLP
 H          = par.horizon;
-LineColors = [.0  .2  .4];          
+LineColors = [.0  .2  .4];
 BandColors = [.7  .7  .7];
 
 %% 1) panel LP-IV
-[b, se] = panel_LP(tmp, FE);
+panOut = panel_LP(tmp, FE);
+Ub_PAN = reshape(panOut.IRUb,1,par.horizon);
+Lb_PAN = reshape(panOut.IRLb,1,par.horizon);
+IR_PAN = reshape(panOut.IR,1,par.horizon);
 
 % plotting
-figure; 
+figure;
 hold on;
-ub = b+1.96*se;
-lb = b-1.96*se;
 fill([1:H, fliplr(1:H)],...
-    [ub fliplr(lb)],...
+    [Ub_PAN fliplr(Lb_PAN)],...
     BandColors,'EdgeColor','none');
-plot(1:H, b,'LineWidth',1.2,'color',LineColors);            
-yline(0,'k','LineWidth',.7);  
+plot(1:H, IR_PAN,'LineWidth',1.2,'color',LineColors);
+yline(0,'k','LineWidth',.7);
 xlim([1 H]); axis tight
 set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
     'FontSize',8,'Layer','top')
 hold off;
-
+saveas(gcf,strcat('./output/EMP/EMP_',spec,'PAN.png'));
 
 
 %% 2) Individual LP-IV
-[b_id, se_id, F_id] = ind_LP(tmp);
-mean(F_id)
+indOut = ind_LP(tmp);
+Ub_IND = reshape(indOut.IRUb,par.N,par.horizon);
+Lb_IND = reshape(indOut.IRLb,par.N,par.horizon);
+IR_IND = reshape(indOut.IR,par.N,par.horizon);
 
+% Average Instrument Strength
+fprintf('Average First-stage F stat: %f \n', mean(indOut.F))
+
+% All IR plots
 figure;
 hold on;
-plot(1:H, b_id','LineWidth',1.2,'color',LineColors); 
-yline(0,'k','LineWidth',.7);  
+plot(1:H, IR_IND,'LineWidth',1.2,'color',LineColors);
+yline(0,'k','LineWidth',.7);
 xlim([1 H]); axis tight
 set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
     'FontSize',8,'Layer','top')
 hold off;
+saveas(gcf,strcat('./output/EMP/EMP_',spec,'IND.png'));
 
-
-
-figure; 
-hold on;
-id =211; % Los Angeles
-ub = b_id(id,:)+1.96*se_id(id,:);
-lb = b_id(id,:)-1.96*se_id(id,:);
-fill([1:H, fliplr(1:H)],...
-    [ub fliplr(lb)],...
-    BandColors,'EdgeColor','none');
-plot(1:H, b_id(id,:),'LineWidth',1.2,'color',LineColors);            
-yline(0,'k','LineWidth',.7);  
-xlim([1 H]); axis tight
-set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
-    'FontSize',8,'Layer','top')
-hold off;
-
-
-figure; 
-hold on;
-id =357; % Los Angeles
-ub = b_id(id,:)+1.96*se_id(id,:);
-lb = b_id(id,:)-1.96*se_id(id,:);
-fill([1:H, fliplr(1:H)],...
-    [ub fliplr(lb)],...
-    BandColors,'EdgeColor','none');
-plot(1:H, b_id(id,:),'LineWidth',1.2,'color',LineColors);            
-yline(0,'k','LineWidth',.7);  
-xlim([1 H]); axis tight
-set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
-    'FontSize',8,'Layer','top')
-hold off;
+% Selected IR plots
+for id = [211,357]
+    figure;
+    hold on;
+    fill([1:H, fliplr(1:H)],...
+        [Ub_IND(id,:) fliplr(Lb_IND(id,:))],...
+        BandColors,'EdgeColor','none');
+    plot(1:H, IR_IND(id,:),'LineWidth',1.2,'color',LineColors);
+    yline(0,'k','LineWidth',.7);
+    xlim([1 H]); axis tight
+    set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
+        'FontSize',8,'Layer','top')
+    hold off;
+    saveas(gcf,strcat('./output/EMP/EMP_',spec,'IND_',MSA{1,id},'.png'));
+end
 
 %% 3) Group LP
-Gmax   = 10;
+Gmax   = 8;
 nInit  = 100;
-bInit  = b_id;
-weight = se_id;
-inference = 3;
-[Gr_EST, GIRF, GSE, IC] = GroupLPIV(tmp, Gmax, nInit, bInit, weight, FE, inference);
+bInit  = indOut.b;
+weight = indOut.asymV;
+inference = 1;
+[Gr_EST, GIRF, GSE, OBJ, IC] = GLP(tmp, Gmax, nInit, bInit, weight, FE, inference);
 
-save('./output/EMP/EMP_FE_Y.mat');
+save(strcat('./output/EMP/EMP_',spec,'.mat'));
 
-
-Group = emp_relabel_plot(FE, par, Gr_EST, GIRF, GSE);
-
-%% Plot IC
+% information criterion
 figure;
 plot(IC,'b-s','LineWidth',2,'MarkerSize',5,...
-'MarkerEdgeColor','blue',...
-'MarkerFaceColor','blue');xlabel('Number of Groups');
+    'MarkerEdgeColor','blue',...
+    'MarkerFaceColor','blue');
+xlabel('Number of Groups');
+saveas(gcf,strcat('./output/EMP/EMP_',spec,'IC.png'));
 
-%% Exisitng Criteria: Ad-hoc panel LP
-% get the rich and poor index from data/MSA_Feature.xlsx, there are four
-% sheets
-% rich MSAs
-idx = rich;
-idx = kron(idx,ones(reg.param.T,1));idx = logical(idx);
-adhoc.y = reg.y(idx,:);
-adhoc.x = reg.x(idx,:);
-adhoc.z = reg.z(idx,:);
-adhoc.LHS = cumsum(reg.LHS(idx,:),2);
-adhoc.control = reg.control(idx,:);
-adhoc.param.T = reg.param.T;
-adhoc.param.N = sum(rich==1);
+% relabel the groups and plot them
+Group_relabel = nan(par.N,Gmax);
+for Ghat = 1:Gmax
+    girf   = squeeze(GIRF{1,Ghat});
+    gse    = squeeze(GSE{1,Ghat});
+    Ub_GLP = girf + 1.96*gse;
+    Lb_GLP = girf - 1.96*gse;
 
-[b_ad, se_ad] = panel_LP(adhoc,0);
+    % order ir by positiveness
+    [~,ord] = sort(mean(girf,2),'descend');
+    if Ghat>=2 && Ghat<=4
+        figure;
+        for g = 1:Ghat
+            subplot(ceil(Ghat/2),2,g);
+            k = ord(g);
+            hold on;
+            % bands
+            fill([1:H, fliplr(1:H)],...
+                [Ub_GLP(k,:) fliplr(Lb_GLP(k,:))],...
+                BandColors,'EdgeColor','none');
+            % IR
+            plot(1:H, girf(k,:),'LineWidth',1.2,'color',LineColors);
 
-figure;
-hold on;
-ub = b_ad+1.96*se_ad;
-lb = b_ad-1.96*se_ad;
-fill([1:H, fliplr(1:H)],...
-    [ub fliplr(lb)],...
-    BandColors,'EdgeColor','none');
-plot(1:H, b_ad,'LineWidth',1.2,'color',LineColors);            
-yline(0,'k','LineWidth',.7);  
-xlim([1 H]); axis tight
-set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
-    'FontSize',8,'Layer','top')
-hold off;
-
-
-% poor MSAs
-idx = poorG3_1;
-idx = kron(idx,ones(reg.param.T,1));idx = logical(idx);
-adhoc.y = reg.y(idx,:);
-adhoc.x = reg.x(idx,:);
-adhoc.z = reg.z(idx,:);
-adhoc.LHS = cumsum(reg.LHS(idx,:),2);
-adhoc.control = reg.control(idx,:);
-adhoc.param.T = reg.param.T;
-adhoc.param.N = sum(poorG3_1);
-
-[b_ad, se_ad] = panel_LP(adhoc,0);
-
-figure;
-hold on;
-ub = b_ad+1.96*se_ad;
-lb = b_ad-1.96*se_ad;
-fill([1:H, fliplr(1:H)],...
-    [ub fliplr(lb)],...
-    BandColors,'EdgeColor','none');
-plot(1:H, b_ad,'LineWidth',1.2,'color',LineColors);            
-yline(0,'k','LineWidth',.7);  
-xlim([1 H]); axis tight
-set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
-    'FontSize',8,'Layer','top')
-hold off;
-
+            yline(0,'k','LineWidth',.7);
+            xlabel(strcat({'Group'},{' '},num2str(g)));
+            xlim([1 H]); axis tight
+            set(gca,'XTick',[1 6 12 18 24],'XTickLabel',cellstr(num2str([1 6 12 18 24]')),...
+                'FontSize',8,'Layer','top')
+            hold off
+        end
+        saveas(gcf,strcat('./output/EMP/EMP_',spec,'GIRF_G',num2str(Ghat),'.png'));
+    end
+    % store ordered group classification
+    gr_tmp = zeros(par.N,1);
+    for g = 1:Ghat
+        gr_tmp = gr_tmp +(Gr_EST(:,Ghat) == ord(g) )*g;
+    end
+    Group_relabel(:,Ghat) = gr_tmp; % store it for later output
+end
+writematrix(Group_relabel,strcat('./output/EMP/EMP_',spec,'Gr_re.csv'));
