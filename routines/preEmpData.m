d6b8a890d217fcb00d20fd0f4e2c9b87509c5ca3
Jiaming Huang <jiaming.huang@upf.edu>
2022-04-13 16:32:59 +0200

    replace prepare_GLP

--- routines/preEmpData.m
+++ routines/preEmpData.m
@@ -1,34 +1,40 @@
-function reg = prepare_GLP(data, par)
-% given the index of variables, extract them, prepare lags and drop missings
-% INPUT:
-% -- data: data matrix of size NT by p
-% -- par : a struct with 1) the index of y, x, (w and z); 
-%                        2) # of lags
-% by default, x and z should be of column 1!
-
-% OUTPUT:
-% -- reg: data struct with y, x, (and z), LHS, controls and params that can be fed
+function reg = preEmpData(data, par)
+% Prepare data for empirical application: given the index of variables, 
+% extract them, prepare lags and drop missings
+% --------------------------- INPUT --------------------------------
+% data: data matrix of size NT by p (balanced)
+% par : a struct with 1) y_idx, x_idx, c_idx, zx_idx, zc_idx
+%                           the index of y, x, c, zx, and zc; 
+%                     2) nylag, nxlag, nclag, nzlag
+%                           # of lags as additional controls
+%                           by default nzlag should be 0
+%                     3) start, end
+%                           starting & ending dates of form 'yyyy-mm-dd'
+%                     4) N, Tfull, # of units & (full) time periods
+%                     5) date, Tfull by 1 vector of dates
+% --------------------------- OUTPUT --------------------------------
+% reg: data struct with y, x, (and z), LHS, controls and params that can be fed
 %         into GroupLP function
 
-
-
-t_start = find(strcmp(par.date, par.start));
-t_end   = find(strcmp(par.date, par.end));
+%% Step 1: Keep only sampling periods
+idxStart = find(strcmp(par.date, par.start));
+idxEnd   = find(strcmp(par.date, par.end));
 keep    = zeros(par.Tfull,1); 
-keep(t_start:t_end,1) = 1;
+keep(idxStart:idxEnd,1) = 1;
 T       = sum(keep);
 date    = par.date(logical(keep),1);
 keep    = kron(ones(par.N,1),keep);
 data    = data(logical(keep),:);
 
-%% extract variables at time t
+%% Step 2: Extract variables y, x, c, zx, zc
 y = data(:,par.y_idx);
 x = data(:,par.x_idx);
-w = data(:,par.w_idx);   % if we don't have controls, par.w_idx should be []
-z = data(:,par.z_idx);   % same for z_idx
-L = size(par.z_idx,2);     % L by 1 instrument
+c = data(:,par.c_idx);   % if we don't have controls, par.c_idx should be []
+zx = data(:,par.z_idx);
+L = size(zx,2);     % L by 1 instrument
 
-if isempty(w) && isempty(z)
+%% Step 3: Generate lags
+if isempty(c) && isempty(zx)
     %% Simplest model: OLS + no external controls
     ylag = nan(size(y,1),size(y,2)*par.nylag);
     xlag = nan(size(x,1),size(x,2)*par.nxlag); 
@@ -46,80 +52,79 @@ if isempty(w) && isempty(z)
     reg.x = tmp(keep,2);
     reg.LHS = tmp(keep,2+1:2+par.horizon);
     reg.control = tmp(keep,2+par.horizon+1:end);
-    reg.z = [];
     
-elseif isempty(w)
+elseif isempty(c)
     %% LP-IV: but no external controls
     ylag = nan(size(y,1),size(y,2)*par.nylag);
     xlag = nan(size(x,1),size(x,2)*par.nxlag);
-    zlag = nan(size(z,1),size(z,2)*par.nzlag);    
+    zlag = nan(size(zx,1),size(zx,2)*par.nzlag);    
     for i = 1:par.N
         ytmp = y(T*(i-1)+1:T*i,:);
         xtmp = x(T*(i-1)+1:T*i,:);
-        ztmp = z(T*(i-1)+1:T*i,:);
+        ztmp = zx(T*(i-1)+1:T*i,:);
         
         ylag(T*(i-1)+1:T*i,:) = lag(ytmp,par.nylag);
         xlag(T*(i-1)+1:T*i,:) = lag(xtmp,par.nxlag);
         zlag(T*(i-1)+1:T*i,:) = lag(ztmp,par.nzlag);
         ylead(T*(i-1)+1:T*i,:) = lag(ytmp,-par.horizon);
     end
-    tmp = [y x z ylead ylag xlag zlag];
-    keep  = sum(isnan(tmp),2)==0;
-    reg.y = tmp(keep,1);
-    reg.x = tmp(keep,2);
-    reg.z = tmp(keep,3:2+L);
+    tmp = [y x zx ylead ylag xlag zlag];
+    keep    = sum(isnan(tmp),2)==0;
+    reg.y   = tmp(keep,1);
+    reg.x   = tmp(keep,2);
+    reg.zx  = tmp(keep,3:2+L);
     reg.LHS = tmp(keep,2+L+1:2+L+par.horizon);
-    reg.control = tmp(keep,2+L+par.horizon+1:end);
+    reg.c   = tmp(keep,2+L+par.horizon+1:end);
     
-elseif isempty(z)
+elseif isempty(zx)
     %% OLS: with external controls
     ylag = nan(size(y,1),size(y,2)*par.nylag);
     xlag = nan(size(x,1),size(x,2)*par.nxlag);
-    wlag = nan(size(w,1),size(w,2)*par.nwlag);
+    clag = nan(size(c,1),size(c,2)*par.nclag);
     
     for i = 1:par.N
         ytmp = y(T*(i-1)+1:T*i,:);
         xtmp = x(T*(i-1)+1:T*i,:);
-        wtmp = w(T*(i-1)+1:T*i,:);
+        ctmp = c(T*(i-1)+1:T*i,:);
         
         ylag(T*(i-1)+1:T*i,:) = lag(ytmp,par.nylag);
         xlag(T*(i-1)+1:T*i,:) = lag(xtmp,par.nxlag);
-        wlag(T*(i-1)+1:T*i,:) = lag(wtmp,par.nwlag);
+        clag(T*(i-1)+1:T*i,:) = lag(ctmp,par.nclag);
         ylead(T*(i-1)+1:T*i,:) = lag(ytmp,-par.horizon);
     end
-    tmp = [y x ylead w ylag xlag wlag];
+    tmp = [y x ylead c ylag xlag clag];
     keep  = sum(isnan(tmp),2)==0;
     reg.y = tmp(keep,1);
     reg.x = tmp(keep,2);
     reg.LHS = tmp(keep,3:2+par.horizon);
-    reg.control = tmp(keep,2+par.horizon+1:end);
-    reg.z = [];
+    reg.c = tmp(keep,2+par.horizon+1:end);
+
 else
     %% LP-IV: with external controls
     ylag = nan(size(y,1),size(y,2)*par.nylag);
     xlag = nan(size(x,1),size(x,2)*par.nxlag);
-    wlag = nan(size(w,1),size(w,2)*par.nwlag);
-    zlag = nan(size(z,1),size(z,2)*par.nzlag);
+    clag = nan(size(c,1),size(c,2)*par.nclag);
+    zlag = nan(size(zx,1),size(zx,2)*par.nzlag);
     ylead = nan(size(y,1),par.horizon);
     for i = 1:par.N
         ytmp = y(T*(i-1)+1:T*i,:);
         xtmp = x(T*(i-1)+1:T*i,:);
-        wtmp = w(T*(i-1)+1:T*i,:);
-        ztmp = z(T*(i-1)+1:T*i,:);
+        ctmp = c(T*(i-1)+1:T*i,:);
+        ztmp = zx(T*(i-1)+1:T*i,:);
         
         ylag(T*(i-1)+1:T*i,:) = lag(ytmp,par.nylag);
         xlag(T*(i-1)+1:T*i,:) = lag(xtmp,par.nxlag);
-        wlag(T*(i-1)+1:T*i,:) = lag(wtmp,par.nwlag);
+        clag(T*(i-1)+1:T*i,:) = lag(ctmp,par.nclag);
         zlag(T*(i-1)+1:T*i,:) = lag(ztmp,par.nzlag);
         ylead(T*(i-1)+1:T*i,:) = lag(ytmp,-par.horizon);
     end
-    tmp = [y x z ylead w ylag xlag wlag zlag];
+    tmp = [y x zx ylead c ylag xlag clag zlag];
     keep  = sum(isnan(tmp),2)==0;
     reg.y = tmp(keep,1);
     reg.x = tmp(keep,2);
-    reg.z = tmp(keep,3:2+L);
+    reg.zx = tmp(keep,3:2+L);
     reg.LHS = tmp(keep,2+L+1:2+L+par.horizon);
-    reg.control = tmp(keep,2+L+par.horizon+1:end);
+    reg.c = tmp(keep,2+L+par.horizon+1:end);
 end
 
 
