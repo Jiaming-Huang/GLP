ca40a4e2f357b21232c91f0420127f1abb361a64
Jiaming Huang <jiaming.huang@upf.edu>
2022-04-13 16:33:04 +0200

    misc

--- routines/resid.m
+++ routines/resid.m
@@ -1,18 +1,24 @@
 function [dif, converge] = resid(b_old,b_new,Q_old,Q_new,tol)
 % Compute whether the iteration converges
 % Inputs:
-% b_old, b_new: K by h matrix of group coefs
+% b_old, b_new: K by 1 by G by H matrix of group coefs
 % Q_old, Q_new: obj function
 
 % Outputs:
 % dif is the max of the two criteria
 % converge is a dummy indicator 1(converged) 0 (not)
-
-a_nom = sum(sum( vecnorm( abs( b_old - b_new ), 2, 2 ) ));
-a_den = sum(sum( vecnorm( abs( b_old ), 2, 2 ) )) + 0.001;
+G0 = size(b_old,3);
+a_nom = 0;
+a_den = 0;
+for i = 1:G0
+    old = b_old(:,:,i,:);
+    new = b_new(:,:,i,:);
+    a_nom = a_nom + norm(old(:)-new(:));
+    a_den = a_den + norm(old(:));
+end
 
 converge = 0;
-dif = max(abs(Q_new-Q_old), a_nom/a_den);
+dif = max(abs(Q_new-Q_old), a_nom/(a_den+0.001));
 
 if dif < tol
     converge = 1;
